9ddf1bc419ef82b7f9a55afa99acf3d8a6051430
Alessandro Menti <alessandro.menti@studenti.univr.it>
2014-03-07 19:13:24 +0000

    * Added a side light

--- src/it/univr/rubikcube/gui/JRubikCube.java
+++ src/it/univr/rubikcube/gui/JRubikCube.java
@@ -1,8 +1,13 @@
 package it.univr.rubikcube.gui;
 
+import javax.media.j3d.BoundingSphere;
 import javax.media.j3d.BranchGroup;
 import javax.media.j3d.Canvas3D;
+import javax.media.j3d.DirectionalLight;
 import javax.media.j3d.TransformGroup;
+import javax.vecmath.Color3f;
+import javax.vecmath.Point3d;
+import javax.vecmath.Vector3f;
 import com.sun.j3d.utils.geometry.ColorCube;
 import com.sun.j3d.utils.universe.SimpleUniverse;
 
@@ -26,8 +31,6 @@ public class JRubikCube extends Canvas3D {
         super(SimpleUniverse.getPreferredConfiguration());
         // Create the universe
         this.universe = new SimpleUniverse(this);
-        // Move the view back a bit to allow all objects to be viewed
-        this.universe.getViewingPlatform().setNominalViewingTransform();
         // Ensure every frame is at least 5 msec long so that it can be noticed
         this.universe.getViewer().getView().setMinimumFrameCycleTime(5);
         // Create the scene graph.
@@ -38,6 +41,15 @@ public class JRubikCube extends Canvas3D {
         this.scene.addChild(this.sceneTransform);
         // TODO Add all the subcubes here
         this.sceneTransform.addChild(new ColorCube(0.4));
+        // TODO Add the right side light
+        Color3f sideLightColor = new Color3f(0.5f, 0.5f, 0.5f);
+        BoundingSphere sideLightBounds = new BoundingSphere(new Point3d(0.0, 0.0, 0.0), 100.0);
+        Vector3f sideLightDirection = new Vector3f(4.0f, -7.0f, -12.0f);
+        DirectionalLight sideLight = new DirectionalLight(sideLightColor, sideLightDirection);
+        sideLight.setInfluencingBounds(sideLightBounds);
+        this.sceneTransform.addChild(sideLight);
+        // Move the view back a bit to allow all objects to be viewed
+        this.universe.getViewingPlatform().setNominalViewingTransform();
         // Optimize the scene graph and add it to the universe
         this.scene.compile();
         this.universe.addBranchGraph(this.scene);
