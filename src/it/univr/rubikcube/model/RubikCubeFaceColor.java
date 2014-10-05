967fa084ee147a50655e6c11875213851e4b3641
Alessandro Menti <alessandro.menti@studenti.univr.it>
2014-10-05 19:23:07 +0000

    * Added array value for the face color

--- src/it/univr/rubikcube/model/RubikCubeFaceColor.java
+++ src/it/univr/rubikcube/model/RubikCubeFaceColor.java
@@ -8,41 +8,55 @@ import java.awt.Color;
  */
 public enum RubikCubeFaceColor {
     /** White face. */
-    WHITE(new Color(255, 255, 255), 'W'),
+    WHITE(new Color(255, 255, 255), 0, 'W'),
     /** Red face. */
-    RED(new Color(255, 0, 0), 'R'),
+    RED(new Color(255, 0, 0), 1, 'R'),
     /** Blue face. */
-    BLUE(new Color(0, 51, 255), 'B'),
+    BLUE(new Color(0, 51, 255), 2, 'B'),
     /** Orange face. **/
-    ORANGE(new Color(255, 153, 0), 'O'),
+    ORANGE(new Color(255, 153, 0), 3, 'O'),
     /** Green face. */
-    GREEN(new Color(0, 153, 0), 'G'),
+    GREEN(new Color(0, 153, 0), 4, 'G'),
     /** Yellow face. */
-    YELLOW(new Color(255, 255, 0), 'Y');
+    YELLOW(new Color(255, 255, 0), 5, 'Y');
     /**
-     * RGB color associated with the face.
+     * RGB color associated with the color.
      */
     private Color rgbColor;
     /**
-     * Color letter associated with this face.
+     * Integer array value associated with this color.
+     */
+    private int value;
+    /**
+     * Color letter associated with this color.
      */
     private char colorLetter;
     /**
      * Creates a new Rubik cube face color associated with the specified RGB
      * color.
-     * @param c RGB color associated with the face.
+     * @param c RGB color associated with the color.
+     * @param i Integer array value associated with this color.
      * @param l Letter identifying the color.
      */
-    RubikCubeFaceColor(final Color c, final char l) {
+    RubikCubeFaceColor(final Color c, final int i, final char l) {
         this.rgbColor = c;
+        this.value = i;
         this.colorLetter = l;
     }
     /**
-     * Gets the color associated with this face.
-     * @return RGB color associated with this face.
+     * Gets the color associated with this color.
+     * @return RGB color associated with this color.
      */
     public final Color getColor() {
         return this.rgbColor;
+    }
+    /**
+     * Gets the integer array value associated with this color. Used when
+     * indexing the arrays by color.
+     * @return Integer array value associated with this color.
+     */
+    public final int getValue() {
+        return this.value;
     }
     /**
      * Gets the letter identifying the color.
